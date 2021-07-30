3765d6cd417fb04b581b51637c842cdf854f32f8
cyr9210 <jipsyer@naver.com>
2021-07-31 00:03:53 +0900

    init_cascase_orphanremoval

---
+++ cascade_orphanremoval/src/main/java/com/bong/cascade_orphanremoval/CascadeOrphanremovalApplication.java
@@ -0,0 +1,13 @@
+package com.bong.cascade_orphanremoval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadeOrphanremovalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CascadeOrphanremovalApplication.class, args);
+  }
+
+}
