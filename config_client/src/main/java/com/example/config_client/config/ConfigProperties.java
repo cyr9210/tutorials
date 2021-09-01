53a7a95e17b58904cfb6e1d1f9087d2335945d81
cyr9210 <jipsyer@naver.com>
2021-09-01 11:23:10 +0900

    fix # dev properteis change

--- config_client/src/main/java/com/example/config_client/config/ConfigProperties.java
+++ config_client/src/main/java/com/example/config_client/config/ConfigProperties.java
@@ -1,13 +1,14 @@
 package com.example.config_client.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "custom")
 @RefreshScope
-@Getter
+@Getter @Setter
 @Component
 public class ConfigProperties {
 
